7180c255f6c9c596649d1828d74aef0e00c8ed15
Dendi <rixsitillaevt@gmail.com>
2022-10-17 11:37:06 +0500

    docs: implement simple-chain function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,58 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  stack: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    this.stack.length;
+    return this;
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    if (value === undefined) {
+      this.stack.push(`(  )`);
+    } else this.stack.push(`~~( ${value} )`);
+    return this;
+
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    if (
+      typeof position !== "number" ||
+      position === 0 ||
+      position < 0 ||
+      !Number.isInteger(position) ||
+      position > this.stack.length - 1
+    ) {
+      this.stack = [];
+      throw new Error(`You can't remove incorrect link!`);
+    } else {
+      this.stack.splice(position - 1, 1);
+      return this;
+    }
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    this.stack.reverse();
+    return this;
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    let a = this.stack.join("").substring(2);
+    this.stack = [];
+    return a;
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
-  }
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
